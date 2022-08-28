8a14c4a855ccc4b328c43897777d6d07f2eb3a5e
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-28 12:56:02 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -5,8 +5,8 @@ import aiofiles
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 async def gen_thumb(videoid):
-    if os.path.isfile(f"cache/techz{videoid}.png"): 
-        return f"cache/techz{videoid}.png"
+    if os.path.isfile(f"cache/zuesloduop{videoid}.png"): 
+        return f"cache/zuesloduop{videoid}.png"
 
     url = f"https://zuesloduop.herokuapp.com//thumb?videoid={videoid}&botname={MUSIC_BOT_NAME}"
     
@@ -14,10 +14,10 @@ async def gen_thumb(videoid):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"cache/techz{videoid}.png", mode="wb" ) 
+                    f = await aiofiles.open(f"cache/zuesloduop{videoid}.png", mode="wb" ) 
                     await f.write(await resp.read()) 
                     await f.close()
 
-        return f"cache/techz{videoid}.png"
+        return f"cache/zuesloduop{videoid}.png"
     except Exception:
         return YOUTUBE_IMG_URL
