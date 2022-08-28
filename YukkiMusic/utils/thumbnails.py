009eff48a312afb8b8eb5ee12ce8854b0f401e2a
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-28 13:29:42 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -19,6 +19,6 @@ async def gen_thumb(videoid):
                     await f.write(await resp.read()) 
                     await f.close()
 
-        return f"cache/zuesloduop{videoid}.png"
+        return f"cache/musicapii{videoid}.png"
     except Exception:
         return YOUTUBE_IMG_URL
