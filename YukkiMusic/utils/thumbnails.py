67f2e2eca3fd5dad9543cc2e9fd28788e0559e0a
Suru <107604575+Suruxhub@users.noreply.github.com>
2022-08-28 13:13:03 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -1,2 +1,24 @@
 
-import os import aiohttp import aiofiles from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL async def gen_thumb(videoid): if os.path.isfile(f"cache/techz{videoid}.png"): return f"cache/techz{videoid}.png" url = f"https://zeusloduop.herokuapp.com/thumb?videoid={videoid}&botname={MUSIC_BOT_NAME}" try: async with aiohttp.ClientSession() as session: async with session.get(url) as resp: if resp.status == 200: f = await aiofiles.open(f"cache/techz{videoid}.png", mode="wb" ) await f.write(await resp.read()) await f.close() return f"cache/techz{videoid}.png" except Exception: return YOUTUBE_IMG_URL
+import os
+import aiohttp
+import aiofiles
+
+from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+
+async def gen_thumb(videoid):
+    if os.path.isfile(f"cache/techz{videoid}.png"): 
+        return f"cache/techz{videoid}.png"
+
+    url = f"https://zuesloduop.herokuapp.com//thumb?videoid={videoid}&botname={MUSIC_BOT_NAME}"
+    
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"cache/zuesloduop{videoid}.png", mode="wb" ) 
+                    await f.write(await resp.read()) 
+                    await f.close()
+
+        return f"cache/zuesloduop{videoid}.png"
+    except Exception:
+        return YOUTUBE_IMG_URL
